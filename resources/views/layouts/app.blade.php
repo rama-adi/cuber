108aec9bee21cb7efae22365141d366fbfd9a08e
Rama Adi Nugraha <ramatabanan@gmail.com>
2021-12-15 11:23:54 +0800

    :construction: fix appname
    Signed-off-by: Rama Adi Nugraha <ramatabanan@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{config('app.name')}}</title>
 
     <!-- Styles -->
     <style>[x-cloak] {
